b7606af14cd360873319fed7786958ee28e3bce5
Getnat Ejigu <getnatejigu@github.com>
2016-05-20 20:52:31 -0400

    update JS

--- light_sensor.js
+++ light_sensor.js
@@ -2,6 +2,42 @@
 var accessToken = "92d261ccb8baa27e84f48a9c77306de752111a39";
 var deviceID = "240028000447343138333038";
 var url = "https://api.spark.io/v1/devices/" + deviceID + "/volts";
+var url1 = "https://api.spark.io/v1/devices/" + deviceID + "/relaycontrol";
+
+var g = new JustGage({
+					 id: "gauge",
+					value: 0.0,
+			        min: 0.0,
+					max: 3.3,
+					title: "Volts",
+					label: "volts",
+					decimals: 2
+					});
+
+function toggleState(item, relayNumber){
+	function setRelay(message){
+		$.post(url1, {params: message, access_token: accessToken}, callback);
+	}  
+	function callback(data, status){
+		if (data.return_value == 1) {
+			if (item.className == "on") {
+				item.className="off";
+			}
+			else {
+				item.className="on";
+			}
+		}
+		else
+		{
+			alert("Could not Connect to Photon");
+		}
+	}
+   if(item.className == "on") {
+	  setRelay(relayNumber + "0");
+   } else {
+	  setRelay(relayNumber + "1");
+   }
+}
 
 function callback(data, status) {
 	if(status == "success") {
@@ -20,15 +56,6 @@ function getReading() {
 }
 
 
-var g = new JustGage({
-					 id: "gauge",
-					value: 0.0,
-			        min: 0.0,
-					max: 3.3,
-					title: "Volts",
-					label: "volts",
-					decimals: 2
-					});
 getReading();
 
 //hello
