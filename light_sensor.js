05d646ae8f9b3490968860098d5d11e3e2989e48
Getnat Ejigu <getnatejigu@github.com>
2016-05-20 19:04:13 -0500

    initial commit

---
+++ light_sensor.js
@@ -0,0 +1,10 @@
+var g = new JustGage({
+					 id: "gauge",
+					value: 0.0,
+			        min: 0.0,
+					max: 3.3,
+					title: "Volts",
+					label: "volts",
+					decimals: 2
+					});
+getReading();
